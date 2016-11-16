691d7f3314e5864f0001eb7eebc1014acab29b36
Berryhill <matthewberryhill@gmail.com>
2016-11-15 19:09:28 -0700

    adding ic2 2.1

--- main.go
+++ main.go
@@ -8,34 +8,39 @@ import (
 	"time"
 	
 	"github.com/kidoman/embd"
-	_ "github.com/kidoman/embd/host/all"
+	//_ "github.com/kidoman/embd/host/all"
 )
+//
+//const (
+//	address = 0x77
+//
+//	calAc1          = 0xAA
+//	calAc2          = 0xAC
+//	calAc3          = 0xAE
+//	calAc4          = 0xB0
+//	calAc5          = 0xB2
+//	calAc6          = 0xB4
+//	calB1           = 0xB6
+//	calB2           = 0xB8
+//	calMB           = 0xBA
+//	calMC           = 0xBC
+//	calMD           = 0xBE
+//	control         = 0xF4
+//	tempData        = 0xF6
+//	pressureData    = 0xF6
+//	readTempCmd     = 0x2E
+//	readPressureCmd = 0x34
+//
+//	tempReadDelay = 5 * time.Millisecond
+//
+//	p0 = 101325
+//
+//	pollDelay = 250
+//)
+
 
 const (
-	address = 0x77
-
-	calAc1          = 0xAA
-	calAc2          = 0xAC
-	calAc3          = 0xAE
-	calAc4          = 0xB0
-	calAc5          = 0xB2
-	calAc6          = 0xB4
-	calB1           = 0xB6
-	calB2           = 0xB8
-	calMB           = 0xBA
-	calMC           = 0xBC
-	calMD           = 0xBE
-	control         = 0xF4
-	tempData        = 0xF6
-	pressureData    = 0xF6
-	readTempCmd     = 0x2E
-	readPressureCmd = 0x34
-
-	tempReadDelay = 5 * time.Millisecond
-
-	p0 = 101325
-
-	pollDelay = 250
+	i2c_SLAVE = 0x0703
 )
 
 func main() {
@@ -46,38 +51,17 @@ func main() {
 	//
 	//spiBus := embd.NewSPIBus(embd.SPIMode0, 0, 1000000, 8, 0)
 	//defer spiBus.Close()
-
-	flag.Parse()
-
-	if err := embd.InitI2C(); err != nil {
-		panic(err)
-	}
-	defer embd.CloseI2C()
-
-	i2cBus := embd.NewI2CBus(0)
-
 	//dataByte := byte(85)
-	fmt.Println("starting")
 
-	for k := 0; k < 100000; k++ { 
+	fmt.Println("starting")
+	i2cBus := i2c.New(0x0AA, 1)
+
+	for {
 		//dataReceived, err := spiBus.TransferAndReceiveByte(dataByte)
 		//if err != nil {
 		//	panic(err)
 		//}
-
-		if err :=  i2cBus.WriteByteToReg(address, control, readTempCmd); err != nil {
-			panic(err)
-		}
-		time.Sleep(50 * time.Millisecond)
-		temp, err := i2cBus.ReadWordFromReg(address, tempData)
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Println(temp)
-		//fmt.Println(dataReceived)
-		time.Sleep(50 * time.Millisecond)
-
 	}
 }
+
 
