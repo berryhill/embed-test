bba5c9cfc03134cadc3a866fc3fc04d6889523e8
Berryhill <matthewberryhill@gmail.com>
2016-11-15 17:43:06 -0700

    straitening out dev env

--- main.go
+++ main.go
@@ -18,13 +18,6 @@ func main() {
 	spiBus := embd.NewSPIBus(embd.SPIMode0, 0, 1000000, 8, 0)
 	defer spiBus.Close()
 
-//	receivedByte, err := spiBus.ReceiveByte()
-//	if err != nil {
-//		panic(err)
-//	}
-//	fmt.Println("received byte is:", receivedByte)
-//	count := 0
-
 	dataByte := byte(170)
 	fmt.Println("starting")
 
