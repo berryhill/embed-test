25ca838bfee4351777f9489c5899b4b83da6bdc0
Berryhill <matthewberryhill@gmail.com>
2016-11-15 18:42:21 -0700

    adding ic2 6

--- main.go
+++ main.go
@@ -12,6 +12,33 @@ import (
 	_ "github.com/kidoman/embd/host/all"
 )
 
+const (
+	address = 0x77
+
+	calAc1          = 0xAA
+	calAc2          = 0xAC
+	calAc3          = 0xAE
+	calAc4          = 0xB0
+	calAc5          = 0xB2
+	calAc6          = 0xB4
+	calB1           = 0xB6
+	calB2           = 0xB8
+	calMB           = 0xBA
+	calMC           = 0xBC
+	calMD           = 0xBE
+	control         = 0xF4
+	tempData        = 0xF6
+	pressureData    = 0xF6
+	readTempCmd     = 0x2E
+	readPressureCmd = 0x34
+
+	tempReadDelay = 5 * time.Millisecond
+
+	p0 = 101325
+
+	pollDelay = 250
+)
+
 func main() {
 	if err := embd.InitSPI(); err != nil {
 		panic(err)
@@ -42,24 +69,17 @@ func main() {
 			panic(err)
 		}
 
-		temp, err := baro.Temperature()
-		if err != nil {
-			panic(err)
+		if err :=  i2cBus.WriteByteToReg(address, control, readTempCmd); err != nil {
+			return 0, err
 		}
-		fmt.Printf("Temp is %v\n", temp)
-		pressure, err := baro.Pressure()
+		time.Sleep(50 * time.Millisecond)
+		temp, err := i2cBus.ReadWordFromReg(address, tempData)
 		if err != nil {
-			panic(err)
+			return 0, err
 		}
-		fmt.Printf("Pressure is %v\n", pressure)
-		altitude, err := baro.Altitude()
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("Altitude is %v\n", altitude)
+		return temp, nil
 
 		fmt.Println(dataReceived)
-
 		time.Sleep(50 * time.Millisecond)
 
 	}
